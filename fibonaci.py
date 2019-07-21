fa2eb1c7b8fcbe06f13355cbbfcf8842277bc65f
Hoang Phan <thanh.hoang.bk@gmail.com>
2019-07-22 05:38:36 +0700

    refine the getopt to improve user experience when inputing the range

--- fibonaci.py
+++ fibonaci.py
@@ -1,11 +1,28 @@
+import sys, getopt
+
+def proc_opt(argv):
+	n = 0
+	try:
+		opts, args = getopt.getopt(argv, "hn:")
+	except getopt.GetoptError:
+		print 'fibonaci.py -n <end number>'
+		sys.exit(2)
+	for opt, arg in opts:
+		if opt == '-h':
+			print 'fibonaci.py -n <end number>'
+		elif opt == '-n':
+			n = int(arg)
+	return n
+
+
 def fib(n):
 	a, b = 0, 1
 	while a < n:
 		print(a)
 		a, b = b, a+b
-	print ()
 
-n = input('Tell me the end number?\n')
+# parsing arguments
+n = proc_opt(sys.argv[1:])
 
-print ('here is the list of fibonaci number up to %d' % n)
+print ('we are about to list all fibonaci number up to %d' % n)
 fib(n)
